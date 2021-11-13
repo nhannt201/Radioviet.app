c626bccacbeb48c30d1838631677f0960d7a068e
nhannt201 <trungnhan21.12@gmail.com>
2021-11-13 08:27:30 +0700

    Open Source

---
+++ app/src/main/java/vn/sun/studio/radio/apps/CustomWebViewClient.java
@@ -0,0 +1,21 @@
+package vn.sun.studio.radio.apps;
+
+import android.net.http.SslError;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class CustomWebViewClient extends WebViewClient {
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        view.loadUrl(url);
+        return true;
+    }
+    @Override
+    public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError er) {
+        handler.proceed();
+        // Ignore SSL certificate errors
+    }
+
+
+}
